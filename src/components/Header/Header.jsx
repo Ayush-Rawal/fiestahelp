4ac1ea10264f53784f58531ec8cb6a3ee090d5dd
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2019-04-05 23:17:10 +0530

    Added Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Link} from '@reach/router'
+import {AuthContext} from '../AuthWall'
+import css from './style.module.css'
+import Navbar from '../Navbar'
+
+import fiestaLogo from '../../images/ff_logo_dark.png'
+
+export function Header() {
+    return (
+        <header className={css.header}>
+            <Link to="/" className={css.logo__link}>
+                <section className={css.logo}>
+                    <img src={fiestaLogo} className={css.logo__image} alt="Flair Fiesta Logo"/>
+                    <div style={{display: "flex", flexFlow: "column wrap"}}>
+                        <h1 className={css.logo__name}>Help Fiesta</h1>
+                        <span className={css.logo__tagline} >The Ambulance System</span>
+                    </div>
+                </section>
+            </Link>
+            <Navbar />
+            <UserProfile />
+        </header>
+    )
+}
+
+function UserProfile () {
+    return (
+        <AuthContext.Consumer>
+            {user => (
+                user.isAuthenticated?
+                (
+                    <div>
+                        Hi {user.user.name},
+                        <button>logout</button>
+                    </div>
+                ) : (
+                    <button>Login</button>                    
+                )
+            )}
+        </AuthContext.Consumer>
+    )
+}
