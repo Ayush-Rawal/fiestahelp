5cb21238d9bb909fe7712bf5cc0c2945df22fe46
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2019-04-06 04:57:57 +0530

    Added server, mongoose connection and passport initialization

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const passport = require("passport")
+const path = require("path")
+const mongoose = require("mongoose")
+
+mongoose.connect('mongodb://localhost:27017/fiesta', {useNewUrlParser: true})
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("DB Connected")
+});
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(session({secret: "Real Secret"}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get("/", function (req, res) {
+    return res.sendFile(path.resolve(__dirname, "./public/index.html"))
+})
+
+app.get("*", function (req, res) {
+    return res.sendFile(path.resolve(__dirname, "./public/index.html"))
+})
+
+app.listen(process.env.port || 8080, () => console.log(`listening on ${process.env.port || 8080}`))
