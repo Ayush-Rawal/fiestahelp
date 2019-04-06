7eaf2295fb264d41ce3f8fb84a71d43852e5afc3
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2019-04-06 08:38:37 +0530

    Completed AuthWall

--- src/components/AuthWall/AuthWall.jsx
+++ src/components/AuthWall/AuthWall.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
 
 let defaultAuth = {
-    isAuthenticated: false,
-    user: {
-        name: "",
-        isModerator: false
-    },
-    logout: () => {}
-}
+  isAuthenticated: false,
+  user: {
+    name: "",
+    isModerator: false
+  }
+};
 
-export let AuthContext = React.createContext(defaultAuth)
+export let AuthContext = React.createContext(defaultAuth);
 
-export function AuthWall({children}) {
-
-    return (
-            <AuthContext.Provider value={defaultAuth}>
-                    {children}
-            </AuthContext.Provider>            
-    )
+export function AuthWall({ children }) {
+  const [auth, setAuth] = useState(defaultAuth);
+  function logout() {
+    setAuth(defaultAuth);
+  }
+  function setUser(name, isModerator) {
+    setAuth({
+      isAuthenticated: true,
+      user: {
+          name,
+          isModerator
+      }
+    });
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        ...auth,
+        logout,
+        setUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
