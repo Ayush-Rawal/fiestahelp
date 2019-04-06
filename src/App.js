5ee63c4a27fac6a71c55f6ba9254c1925522791e
Ayush Rawal <31813648+Ayush-Rawal@users.noreply.github.com>
2019-04-06 09:32:59 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -10,24 +10,26 @@ import Ambulances from './components/Ambulances'
 import Landing from './components/Landing'
 import LoginSignup from './components/LoginSignup'
 import ModeratorLogin from './components/ModeratorLogin'
+import ModeratorDashboard from './components/ModeratorDashboard'
 class App extends Component {
   render() {
     return (
-      <Layout>
         <AuthWall>
+          <Layout>
             <AuthContext.Consumer>
               {auth => (
               <Router>
                 {auth.isAuthenticated && <Landing path="/"/>}
                 {auth.isAuthenticated && <Ambulances path="/ambulances"/>}
                 {auth.isAuthenticated && <NotFound default />}
+                {auth.isAuthenticated && auth.user.isModerator && <ModeratorDashboard path="/moderator" />}
                 {!auth.isAuthenticated && <ModeratorLogin path="/moderator" />}
                 {!auth.isAuthenticated && <LoginSignup path="*" />}
               </Router>
               )}
             </AuthContext.Consumer>
+          </Layout>
         </AuthWall>
-      </Layout>
     );
   }
 }
